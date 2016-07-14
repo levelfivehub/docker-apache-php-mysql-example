386acd5a5b32803b913b078129fcd23344e62522
Gaurav Malhotra <gaurav@level5websites.com>
2016-07-14 22:56:41 +0100

    Update run_dev.sh

--- run_dev.sh
+++ run_dev.sh
@@ -3,3 +3,6 @@ docker-machine create dockertest --driver virtualbox
 docker-machine start dockertest
 eval $(docker-machine env dockertest)
 docker-compose up -d
+
+IP="$(docker-machine ip)"
+echo "${IP} is running, save to etc/hosts file"
